20ffa1d4159148b305ac388e98f1c35e5290bdea
paula-alonso <paula.alonso@correo.unimet.edu.ve>
2024-10-21 16:31:18 -0400

    fix: queue and node were empty

--- Pelea de sagas/src/EDD/Nodo.java
+++ Pelea de sagas/src/EDD/Nodo.java
@@ -8,6 +8,45 @@ package EDD;
  *
  * @author alons
  */
-public class Nodo {
+public class Nodo<T> {
+    private T data;
+    private Nodo<T> pNext;
     
+    /**
+     * Metodo constructor parametrizado
+     * @param data Objeto contenido dentro del nodo
+     */
+
+    public Nodo(T data) {
+        this.data = data;
+        this.pNext = null;
+    }
+        
+    /**
+     * @return the data
+     */
+    public T getData() {
+        return data;
+    }
+
+    /**
+     * @param data the data to set
+     */
+    public void setData(T data) {
+        this.data = data;
+    }
+
+    /**
+     * @return the pNext
+     */
+    public Nodo<T> getpNext() {
+        return pNext;
+    }
+
+    /**
+     * @param pNext the pNext to set
+     */
+    public void setpNext(Nodo<T> pNext) {
+        this.pNext = pNext;
+    }
 }
